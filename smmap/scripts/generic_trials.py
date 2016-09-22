c6888756d28fde61b479594a49da1db0539f3e2d
Dale McConachie <dmcconachie@wpi.edu>
2016-09-22 16:58:09 -0400

    Modified scripts for better useability.

--- smmap/scripts/generic_trials.py
+++ smmap/scripts/generic_trials.py
@@ -3,7 +3,7 @@
 from run_trial import *
 
 
-def run_wafr_trials(experiment, run_baseline=False, run_UCB=False, run_KFMANB=False, run_KFRDB=False, optimization_enabled=None, log_prefix=""):
+def run_trials(experiment, run_baseline=False, run_UCB=False, run_KFMANB=False, run_KFRDB=False, optimization_enabled=None, log_prefix=""):
 
     if run_baseline:
         # Note that this is 0 to 25 as range does [start, stop), thus we get 0:4:24 in Matlab speak
@@ -14,21 +14,13 @@ def run_wafr_trials(experiment, run_baseline=False, run_UCB=False, run_KFMANB=Fa
             for rotational_deform in deform_range:
                 run_trial(experiment=experiment,
                           logging_enabled="true",
-                          test_id=log_prefix + "wafr_paper_trials/" + "single_model_baseline/" + "trans_" + str(translational_deform) + "_rot_" + str(rotational_deform),
+                          test_id=log_prefix + "single_model_baseline/" + "trans_" + str(translational_deform) + "_rot_" + str(rotational_deform),
                           planning_horizon=1,
                           optimization_enabled=optimization_enabled,
                           multi_model="false",
                           deformability_override="true",
                           translational_deformability=translational_deform,
                           rotational_deformability=rotational_deform)
-
-        # Run the single manually tuned version
-        # run_trial(experiment=experiment,
-        #           logging_enabled="true",
-        #           test_id=log_prefix + "wafr_paper_trials/" + "single_model_baseline/" + "manually_tuned",
-        #           planning_horizon=1,
-        #           optimization_enabled=optimization_enabled,
-        #           multi_model="false")
 
         # Note that this is 0 to 16 as range does [start, stop), thus we get 0:1:10 in Matlab speak
         adaptive_range = range(0, 11, 1)
@@ -38,7 +30,7 @@ def run_wafr_trials(experiment, run_baseline=False, run_UCB=False, run_KFMANB=Fa
             adaptive_model_learning_rate = 10.0 ** (-adaptive_exponent)
             run_trial(experiment=experiment,
                       logging_enabled="true",
-                      test_id=log_prefix + "wafr_paper_trials/" + "single_model_baseline/" + "adaptive_1e-" + str(adaptive_exponent),
+                      test_id=log_prefix + "single_model_baseline/" + "adaptive_1e-" + str(adaptive_exponent),
                       planning_horizon=1,
                       optimization_enabled=optimization_enabled,
                       multi_model="false",
@@ -49,7 +41,7 @@ def run_wafr_trials(experiment, run_baseline=False, run_UCB=False, run_KFMANB=Fa
     if run_UCB:
         run_trial(experiment=experiment,
                   logging_enabled="true",
-                  test_id=log_prefix + "wafr_paper_trials/" + "multi_model_UCB_regret",
+                  test_id=log_prefix + "/multi_model_UCB_regret",
                   planning_horizon=1,
                   optimization_enabled=optimization_enabled,
                   bandit_algorithm="UCB",
@@ -60,7 +52,7 @@ def run_wafr_trials(experiment, run_baseline=False, run_UCB=False, run_KFMANB=Fa
         for i in range(0, 10):
             run_trial(experiment=experiment,
                       logging_enabled="true",
-                      test_id=log_prefix + "wafr_paper_trials/" + "multi_model_KFMANB_regret_" + str(i),
+                      test_id=log_prefix + "/multi_model_KFMANB_regret_" + str(i),
                       planning_horizon=1,
                       optimization_enabled=optimization_enabled,
                       bandit_algorithm="KFMANB",
@@ -72,7 +64,7 @@ def run_wafr_trials(experiment, run_baseline=False, run_UCB=False, run_KFMANB=Fa
         for i in range(0, 10):
             run_trial(experiment=experiment,
                       logging_enabled="true",
-                      test_id=log_prefix + "wafr_paper_trials/" + "multi_model_KFRDB_regret_" + str(i),
+                      test_id=log_prefix + "/multi_model_KFRDB_regret_" + str(i),
                       planning_horizon=1,
                       optimization_enabled=optimization_enabled,
                       bandit_algorithm="KFRDB",
