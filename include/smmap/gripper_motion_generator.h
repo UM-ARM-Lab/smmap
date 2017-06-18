1651facfd452f2ae6963d527818a1929bb608d18
Mengyao Ruan <ruanmiao@umich.edu>
2017-06-18 17:17:07 -0400

    Working on gripper_motion_generator

--- include/smmap/gripper_motion_generator.h
+++ include/smmap/gripper_motion_generator.h
@@ -2,6 +2,10 @@
 #define GRIPPER_MOTION_GENERATOR_H
 
 #include "smmap/deformable_model.h"
+#include "smmap/grippers.hpp"
+
+#include "sdf_tools/sdf.hpp"
+#include "smmap/robot_interface.hpp"
 
 namespace smmap {
 
@@ -10,7 +14,37 @@ namespace smmap {
         public:
             GripperMotionGenerator();
 
+            GripperCollisionCheckFunctionType createGripperCollisionCheckFunction();
+
+            AllGrippersSinglePoseDelta samplingGripperMotion();
+
+            //////////////////////////////////////////////////////////////////
+            // Called from outside to find the optimal gripper command
+            //////////////////////////////////////////////////////////////////
+            AllGrippersSinglePoseDelta findOptimalGripperMotion();
+
+
+        private:
+
+            ros::NodeHandle nh_;
+
             DeformableModel::Ptr deformable_model_;
+            sdf_tools::SignedDistanceField enviroment_sdf_;
+            GripperCollisionChecker gripper_colision_checker_;
+
+            AllGrippersSinglePoseDelta grippers_pose_delta_;
+            std::vector<GripperData> grippers_data_;
+
+            const ssize_t num_samples_;
+            const double rotation_lower_bound_;
+            const double rotation_upper_bound_;
+            const double translation_lower_bound_;
+            const double translation_upper_bound_;
+
+//            Visualizer& vis_;
+
+
+//            const GripperMotionLoggingFunctionType logging_fn_;
 
 
 
--- src/gripper_motion_generator.cpp
+++ src/gripper_motion_generator.cpp
@@ -1,4 +1,8 @@
+#include <deformable_manipulation_experiment_params/ros_params.hpp>
+#include <arc_utilities/eigen_helpers_conversions.hpp>
+
 #include "smmap/gripper_motion_generator.h"
+#include "smmap/task.h"
 
 using namespace smmap;
 
@@ -6,3 +10,35 @@ GripperMotionGenerator::GripperMotionGenerator()
 {
 
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions that are used to initialize function pointers in the
+// constructor. These all require that task_type_ and
+// deformable_type_ have been set already
+////////////////////////////////////////////////////////////////////////////////
+
+GripperCollisionCheckFunctionType GripperMotionGenerator::createGripperCollisionCheckFunction()
+{
+    return std::bind(&RobotInterface::checkGripperCollision,
+                     &robot_,
+                     std::placeholders::_1);
+}
+
+AllGrippersSinglePoseDelta GripperMotionGenerator::sampleGripperMotion()
+{
+
+}
+
+
+
+AllGrippersSinglePoseDelta GripperMotionGenerator::findOptimalGripperMotion()
+{
+    for (int ind_sample = 0; ind_sample < num_samples_; ind_sample++)
+    {
+        AllGrippersSinglePoseDelta sample_grippers_motion = samplingGripperMotion();
+
+    }
+
+}
+
+
