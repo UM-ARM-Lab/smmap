73ad864737eb63540762a7004e49b17b2f8048e4
Mengyao Ruan <ruanmiao@umich.edu>
2017-08-03 21:51:37 -0400

    add stretching vector information struct in gripperdata. TODO: set up call back in custom_scence. Be care of ROPE and CLOTH

--- include/smmap/grippers.hpp
+++ include/smmap/grippers.hpp
@@ -31,6 +31,26 @@ namespace smmap
     typedef kinematics::VectorVector6d AllGrippersSinglePoseDelta;
     typedef std::vector<AllGrippersSinglePoseDelta> AllGrippersPoseDeltaTrajectory;
 
+    // Stretching tracking vector information --- Added by Mengyao
+    struct StretchingVectorInfo
+    {
+        StretchingVectorInfo()
+        {}
+
+        StretchingVectorInfo(
+                const std::string& to_gripper_name,
+                const std::vector<long>& from_nodes,
+                const std::vector<long>& to_nodes)
+            : to_gripper_name_(to_gripper_name)
+            , from_nodes_(from_nodes)
+            , to_nodes_(to_nodes)
+        {}
+
+        std::string to_gripper_name_;
+        std::vector<long> from_nodes_;
+        std::vector<long> to_nodes_;
+    };
+
     struct GripperData
     {
         GripperData(const std::string& name, const std::vector<long>& node_indices)
@@ -38,11 +58,26 @@ namespace smmap
             , node_indices_(node_indices)
         {}
 
+        GripperData(const std::string& name,
+                    const std::vector<long>& node_indices,
+                    const std::string& to_gripper_name,
+                    const std::vector<long>& from_nodes,
+                    const std::vector<long>& to_nodes)
+            : name_(name)
+            , node_indices_(node_indices)
+            , stretching_vector_info_(to_gripper_name,
+                                      from_nodes,
+                                      to_nodes)
+        {}
+
         /// The name associated with this gripper
         std::string name_;
 
         /// Vector of the indices of the nodes that are grasped by the gripper
         std::vector<long> node_indices_;
+
+        // Stretching tracking vector information --- Added by Mengyao
+        StretchingVectorInfo stretching_vector_info_;
 
         /**
          * @brief operator <<
