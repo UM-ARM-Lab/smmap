93ff3496c9120a460f7d28ead1e5198c2af1b064
Dale McConachie <dmcconac@umich.edu>
2018-12-24 15:48:04 -0500

    Removed scripts that have been moved to deformable_manipulation_interface.

--- scripts/z_old/test_cloth_table_deformability.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source trial_types.sh
-
-base_environment="cloth_table"
-planning_horizion=1
-base_experiment=$planning_horizion"_step_baseline"
-single_model_trial_multiple_deform_values 10 18 4
