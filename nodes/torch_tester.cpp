89e74c92e8add47402e7513d73a93514dbf18895
Dale McConachie <dmcconac@umich.edu>
2019-09-18 16:19:49 -0400

    Removed need for libtorch hack.

--- nodes/torch_tester.cpp
+++ nodes/torch_tester.cpp
@@ -4,14 +4,15 @@
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "smmap_torch_tester");
+//    ros::init(argc, argv, "smmap_torch_tester");
 
 //    auto nh = std::make_shared<ros::NodeHandle>();
 //    auto ph = std::make_shared<ros::NodeHandle>("~");
 
     torch::jit::script::Module model = torch::jit::load(
-                "/home/dmcconac/Dropbox/catkin_ws/src/smmap/logs/rope_hooks/13feature_torch_model_cpu.pt");
-//    module->to(torch::kCUDA);
+                "/home/dmcconac/Dropbox/catkin_ws/src/smmap/logs/rope_hooks_simple/"
+//                "basic__label_based_on_correct_0.5__normalized_lengths__raw_connected_components__torch_model_cpu.pt");
+                "basic__label_based_on_correct_0.5__normalized_lengths__raw_connected_components__torch_model_cuda.pt");
 
     Eigen::VectorXd vec(13);
     vec << 0.66673756,  0.6558349 ,  0.7069159 ,  0.7233157 ,  0.33333334,  0.33333334,  0.        ,  0.33333334,  0.33333334,  0.        , -1.        , -1.        ,  0. ;
@@ -21,10 +22,8 @@ int main(int argc, char *argv[])
     {
         vec_torch[idx] = vec[idx];
     }
-    std::vector<torch::jit::IValue> const query(1, vec_torch);
-
-//    inputs.push_back(torch::ones({13}).to(torch::kCUDA));
-//    inputs.push_back(torch::ones({13}));
+//    std::vector<torch::jit::IValue> const query(1, vec_torch);
+    std::vector<torch::jit::IValue> const query(1, vec_torch.to(torch::kCUDA));
 
     std::cout << "Output: " << model.forward(query).toTensor().item().toFloat() << std::endl;
 
